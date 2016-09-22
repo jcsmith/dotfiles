1c9e31b477f47260a2431a3593997d2e987510bb
Joshua Smith <jsmith39@systems.wvu.edu>
2016-09-22 13:47:22 -0400

    Update install.sh to manage additional files.  Make some changes to bash configuration to play better with macOS.

--- install.sh
+++ install.sh
@@ -15,3 +15,34 @@ else
 fi
 
 
+############################################################
+#tmux
+############################################################
+
+if [ -e ~/.tmux.conf ]; then
+	echo "~/.tmux.conf already exists."
+else 
+	ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+fi
+
+############################################################
+#bash
+############################################################
+
+if [ -e ~/.bash_profile ]; then
+	echo "~/.bash_profile already exists."
+else 
+	ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+fi
+
+if [ -e ~/.bashrc ]; then
+	echo "~/.bashrc already exists."
+else 
+	ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
+fi
+
+if [ -e ~/.bash_aliases ]; then
+	echo "~/.bash_aliases already exists."
+else 
+	ln -s ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
+fi
